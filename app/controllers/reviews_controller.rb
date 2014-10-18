e559b1d52f5260a446cacba08845e6cba03cf5ea
njdup <njdupp@gmail.com>
2014-10-18 05:24:02 -0700

    Storing sentiment labels and scores for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,5 @@
+require 'HTTParty'
+
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_filter :check_logged_in, only: [:new, :create]
@@ -25,11 +27,18 @@ class ReviewsController < ApplicationController
     @company = Company.find(params[:company_id])
     @review = Review.new(review_params)
     @review.encrypted_key = User.hash_email(current_user.email)
+
+    result = run_sentiment_analysis(@review.content)
+    @review.sentiment_label = result[:sentiment]
+    @review.sentiment_score = result[:score]
+
     if @review.save
         @company.reviews.append(@review)
         @company.save
         flash[:notice] = 'Review was successfully created.' if @review.save
     end
+
+    puts result
     respond_with(@company)
   end
 
@@ -56,5 +65,22 @@ class ReviewsController < ApplicationController
       if !current_user
         redirect_to(new_user_session_path)
       end
+    end
+
+    def run_sentiment_analysis(review_content)
+      api_key = '57a09ad8e5e23b3d3b9e83c780a699e7a1a8650e' #lol secure
+      url = 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment'
+      body = {
+        :apikey => api_key,
+        :text => review_content,
+        :outputMode => 'json',
+        :showSourceText => '1'
+      }
+      response = HTTParty.post(url, :body => body)
+      response_content = JSON.parse response.body
+      result = {
+        :sentiment => response_content["docSentiment"]["type"],
+        :score => response_content["docSentiment"]["score"]
+      }
     end
 end
